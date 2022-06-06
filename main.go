b2ad66e9988447d8f8348554d232403d2f8473f8
Dylan Attal <attal.dylan@gmail.com>
2022-06-06 16:12:41 -0400

    Woah cool learning about specifying data types and that colon equals sign which is shorthand for variable declaration but does not let you specify type.

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	const conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, conferenceName, remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+	// Ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
